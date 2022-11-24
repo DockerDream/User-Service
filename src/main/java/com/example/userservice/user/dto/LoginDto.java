e5354821652f965561c34c6211c7b0a033ce6f6c
ECON <lgyoon621@gmail.com>
2022-11-24 16:37:55 +0900

    [ADD] LoginDto 추가

---
+++ src/main/java/com/example/userservice/user/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.example.userservice.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+    private String code;
+    private String state;
+    private String social;
+
+}
