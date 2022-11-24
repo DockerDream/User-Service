6378be999b3f391684a7e282d5c6b5b578b9cf86
ECON <lgyoon621@gmail.com>
2022-11-24 17:25:17 +0900

    [UPDATE] findByEmail 추가

--- src/main/java/com/example/userservice/user/repository/UserRepository.java
+++ src/main/java/com/example/userservice/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.userservice.user.repository;
 import com.example.userservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
 }
