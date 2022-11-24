72bc2104ae49feec6bd2785834188983e0592ef1
ECON <lgyoon621@gmail.com>
2022-11-24 16:40:33 +0900

    [ADD] ProfileDto 추가

---
+++ src/main/java/com/example/userservice/user/dto/ProfileDto.java
@@ -0,0 +1,13 @@
+package com.example.userservice.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileDto {
+
+    private String profile;
+
+    private String field;
+
+    private int year;
+}
